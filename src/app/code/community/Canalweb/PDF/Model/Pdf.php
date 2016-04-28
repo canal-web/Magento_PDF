a5c44ab15792627ba8d1fc79b1b9e11d892c5392
Fwagra <fabiocerqueiracw@gmail.com>
2016-04-28 18:10:02 +0200

    Generate and store methods

--- src/app/code/community/Canalweb/PDF/Model/Pdf.php
+++ src/app/code/community/Canalweb/PDF/Model/Pdf.php
@@ -1,8 +1,39 @@
 <?php
 class Canalweb_PDF_Model_Pdf
 {
-    public function generatePdf($url, $save = 'save')
+    /**
+     * Generate a pdf from an url and send it
+     * @param string $url
+     * @param string $name
+     */
+    public function generatePdf($url,  $name = "pdf-")
     {
-        
+
+        $pdf = new Pdf($url);
+        $name .= time() . '.pdf';
+
+        if(!$pdf->send($name)){
+            Mage::log('Error generating pdf : ' . $pdf->getError());
+        }
     }
+
+    /**
+     * Generate a pdf from an url and store it
+     * @param string $url
+     * @param string $name
+     * @return string $name
+     */
+    public function savePdf($pdf, $name = "pdf-")
+    {
+        $saveDir = Mage::getBaseDir('var') . '/pdf/';
+        $pdf = new Pdf($url);
+        $name .= time() . '.pdf';
+
+        if(!$pdf->saveAs($saveDir . $name)){
+            Mage::log('Error generating pdf : ' . $pdf->getError());
+        }
+
+        return $name;
+    }
+
 }
